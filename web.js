ada2e6679ef2d52731b91f727ccabad436d2899b
saasbook <saasbook@saasbook.(none)>
2013-07-11 14:26:22 -0700

    w

--- web.js
+++ web.js
@@ -1,7 +1,5 @@
-fs = require('fs');
-fs.readFile('index.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
+var fs = require('fs');
+
+fs.readFile('./index.html',function(err,data){
+  console.log(data.toString()); //replace console.log with the code to write to the response.
 });
