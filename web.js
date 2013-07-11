618aa620e301e9f78717bad186ea1886aef1c73b
saasbook <saasbook@saasbook.(none)>
2013-07-11 14:34:36 -0700

    reverted

--- web.js
+++ web.js
@@ -1,5 +1,28 @@
-var fs = require('fs');
+/*
+var express = require('express');
 
-fs.readFile('./index.html',function(err,data){
-  console.log(data.toString()); //replace console.log with the code to write to the response.
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello  2!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+*/
+
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello  2!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
