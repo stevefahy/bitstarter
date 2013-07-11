4f6e3c5c2db3937fbc44fe544a71490d5628cd81
saasbook <saasbook@saasbook.(none)>
2013-07-11 13:36:06 -0700

    web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('index.html', function(request, response) {
+app.get('/index.html', function(request, response) {
   var k = fs.readFileSync('./index.html','utf8');
   response.send( k );
 });
