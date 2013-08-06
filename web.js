2de97c53293c4f9ee9f19113b3ca31b1565bea3d
stevefahy <stevefahydev@gmail.com>
2013-08-06 12:39:49 -0700

    Updated alt tags and row-fluid classes

--- web.js
+++ web.js
@@ -19,19 +19,12 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-
 app.get('/', function(request, response) {
   var k = fs.readFileSync('./index.html','utf8');
   response.send( k );
 });
 
-/*
-app.get('/', function(request, response) {
-  response.send('Hello  2!');
-});
-*/
-
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
